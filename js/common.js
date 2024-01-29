305f66c3f3ffd83b912e474168429472e3d14796
yama8321 <152832457+yama8321@users.noreply.github.com>
2024-01-29 12:08:02 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
 
-// window.addEventListener('load', function () {
-//     setTimeout(function(){   
-//     let loadingBg = document.querySelector('.loadingWrapper');
-//     loadingBg.classList.add('activeLoad');
-// },3000);
-// });
+window.addEventListener('load', function () {
+    setTimeout(function(){   
+    let loadingBg = document.querySelector('.loadingWrapper');
+    loadingBg.classList.add('activeLoad');
+},3000);
+});
 
 
 const faceBani = document.querySelector('.sec01fbanime');
@@ -58,9 +58,6 @@ function fadeinn() {
         if (targetRect.top < window.innerHeight && targetRect.bottom >= 0) {
             target.classList.add('fadeinn');
         }
-        // else {
-        //     target.classList.remove('fadeinn');
-        // }
     });
 };
 window.addEventListener('scroll', fadeinn);
@@ -85,25 +82,25 @@ window.addEventListener('scroll', bouncebounce);
 function resizeFacebookPlugin() {
     var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    // ウィンドウの幅が500px以下の場合
+
     if (screenWidth <= 500) {
-        // 幅400px、高さ450pxに変更
+
         document.getElementById('my-fb-page').setAttribute('data-width', '300');
         document.getElementById('my-fb-page').setAttribute('data-height', '300');
     } else {
-        // 元の設定に戻す
+
         document.getElementById('my-fb-page').setAttribute('data-width', '400');
         document.getElementById('my-fb-page').setAttribute('data-height', '400');
     }
 
-    // Facebookプラグインを再描画
+
     FB.XFBML.parse();
 }
 
-// ウィンドウのリサイズ時にリサイズ関数を呼び出す
+
 window.addEventListener('resize', resizeFacebookPlugin);
 
-// ページ読み込み時に初回リサイズを実行
+
 window.addEventListener('load', resizeFacebookPlugin);
 
 
@@ -113,22 +110,22 @@ const swiper = new Swiper('.swiper01', {
     effect: 'scroll',
     speed: 1500,
 
-    // If we need pagination
+
     pagination: {
         el: '.swiper-pagination',
     },
 
-    // Navigation arrows
+
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
+
     scrollbar: {
         el: '.swiper-scrollbar',
     },
-    // 自動再生
+
     autoplay: {
         delay: 2000,
     }
@@ -153,24 +150,18 @@ const swiper02 = new Swiper(".swiper02", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-
-    // allowTouchMove: false,
-    // autoheight: true,
-    // slidesPerView: 1,
-    // centeredSlides: true,
-    // spaceBetween: 5,
 });
 
 
 const handslide = document.querySelector('.sec04Hand');
 function handslideanime() {
-    const targetRect = handslide.getBoundingClientRect();
-    if (targetRect.top < window.innerHeight && targetRect.bottom >= 0) {
-        handslide.classList.add('sec04Hand');
-    } else {
-        handslide.classList.remove('sec04Hand');
-    }
-};
+        const targetRect = handslide.getBoundingClientRect();
+        if (targetRect.top < window.innerHeight && targetRect.bottom >= 0) {
+            handslide.classList.add('sec04Hand');
+        } else {
+            handslide.classList.remove('sec04Hand');
+        }
+    };
 
 window.addEventListener('scroll', handslideanime);
 
@@ -184,7 +175,7 @@ window.addEventListener('scroll', handslideanime);
 
 
 const clickbaund = document.querySelector('.sec03click01');
-function handslideanime() {
+function clilkclick() {
     const targetRect = clickbaund.getBoundingClientRect();
     if (targetRect.top < window.innerHeight && targetRect.bottom >= 0) {
         clickbaund.classList.add('sec03click');
@@ -193,7 +184,7 @@ function handslideanime() {
     }
 };
 
-window.addEventListener('scroll', handslideanime);
+window.addEventListener('scroll', clilkclick);
 
 
 
